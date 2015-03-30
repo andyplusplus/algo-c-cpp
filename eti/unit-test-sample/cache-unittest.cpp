b671708b6f734f89e664220668dd1fcd6199cc56
Andy Wu <andyplusplus@gmail.com>
2015-03-29 22:26:35 -0400

    unit-test

--- eti/unit-test-sample/cache-unittest.cpp
+++ eti/unit-test-sample/cache-unittest.cpp
@@ -1,9 +1,10 @@
 /*
   this is to test catch unit test works
+  refer to: https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
  */
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "../catch.hpp"
 
 unsigned int Factorial( unsigned int number ) {
         return number <= 1 ? number : Factorial(number-1)*number;
